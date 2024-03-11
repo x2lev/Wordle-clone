1786ef3b81ae25ab808d8d824a5ee3bd47e21a2a
x2lev <lev.kryvenko@gmail.com>
2024-03-11 11:10:07 -0600

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Kryvenko, Lev on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
